830c4b6f014f3c84a93ebe9b6fc0490659781951
Alex Tiboldi <alex.tiboldi@gmail.com>
2025-02-23 16:44:13 +0300

    update

---
+++ app/dashboard/[projectId]/page.tsx
@@ -0,0 +1,10 @@
+import SchemaViewer from "@/components/pages/project/SchemaViewer";
+import React from "react";
+
+type Props = {};
+
+const Project = (props: Props) => {
+  return <SchemaViewer />;
+};
+
+export default Project;
